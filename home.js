84af67989a45b373ef4f85535da8226dd14b01c0
Roman Malakhin <romanmalakhin@gmail.com>
2017-03-05 19:40:25 +0200

    2.5 add NODE_ENV & definePlugin(for production)

--- home.js
+++ home.js
@@ -2,4 +2,8 @@
 
 let welcome = require('./welcome');
 
+debugger;
 welcome('home');
+
+exports.welcome = welcome;
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,29 @@
+'use strict';
+
+const NODE_ENV =  process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
-    entry: "./home",
+    entry: "./home", //говорит о том какой модуль собирать
     output: {
-        filename: "build.js"
-    }
-}
+        filename: "build.js", //выводить в текущую дерикторию
+        library: "home" //делаем переменную глобальной
+    },
+
+    watch: NODE_ENV == 'development',
+
+    watchOptions: {
+       aggregateTimeout: 100
+    },
+
+    devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : null,
+
+    plugins: [
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        })
+    ]
+
+
+};
+
